dc076d238a84a580417c1df1b0d360c6c018067b
Rajdev-ai <r85107362@gmail.com>
2026-08-09 14:54:36 +0530

    Show professional customer reviews section

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './gallery.css'
 import App from './App.jsx'
+import Reviews from './Reviews.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Reviews />
   </StrictMode>,
 )
