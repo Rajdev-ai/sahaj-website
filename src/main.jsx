7a8ff566e8c3d86d040d8a16b1319014add948ba
Rajdev-ai <r85107362@gmail.com>
2026-08-11 00:17:08 +0530

    Add social links to website

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './gallery.css'
 import './highlights.css'
+import './social-links.css'
 import App from './App.jsx'
 import Reviews from './Reviews.jsx'
+import SocialLinks from './SocialLinks.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SocialLinks />
     <Reviews />
   </StrictMode>,
 )
